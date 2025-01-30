2e370cc705cb3ebd6d9866f6dd842592adb91de9
Rajeev Kumar Singh <rajeevsingh.devops@gmail.com>
2025-01-30 22:05:29 +0530

    Update stop_server.sh
    Created script file in /etc/systemd/system/react-app.service and command here to stop -  systemctl stop react-app.service

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #set errors if any command is not execute successfully
-set -e 
-echo "Appliction stop is underway...."
+#set -e 
+#echo "Appliction stop is underway...."
 #application dir 
-APP_DIR="/home/ubuntu/react-app"
+#APP_DIR="/home/ubuntu/react-app"
 # Port on which our app is running
-PORT=5000
+#PORT=5000
 # find process id 
-PID=$(lsof -t -i:$PORT || true)
+#PID=$(lsof -t -i:$PORT || true)
 #
-if [ -n "$PID" ]; then
-echo "Killing Process"
-kill -9 $PID
-fi
+#if [ -n "$PID" ]; then
+#echo "Killing Process"
+#kill -9 $PID
+#fi
 
-echo "Application Stopped"
-
+#echo "Application Stopped"
+systemctl stop react-app.service
