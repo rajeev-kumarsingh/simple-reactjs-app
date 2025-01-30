df23c4b7e18e30f443fada83c5fb1d82e79ce1f2
Rajeev Kumar Singh <rajeevsingh.devops@gmail.com>
2025-01-30 22:02:51 +0530

    Update start_server.sh
    created  script file in /etc/systemd/system/react-app.service

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,10 @@
 #Application Start
 #!/bin/bash
-set -e 
-echo "Starting Application"
-APP_DIR="/home/ubuntu/react-app/build"
-PORT=5000
-cd $APP_DIR
-nohup serve -s $APP_DIR -l $PORT
-echo "App Stored"
+#set -e 
+#echo "Starting Application"
+#APP_DIR="/home/ubuntu/react-app/build"
+#PORT=5000
+#cd $APP_DIR
+#nohup serve -s $APP_DIR -l $PORT
+#echo "App Stored"
+systemctl start react-app.service
